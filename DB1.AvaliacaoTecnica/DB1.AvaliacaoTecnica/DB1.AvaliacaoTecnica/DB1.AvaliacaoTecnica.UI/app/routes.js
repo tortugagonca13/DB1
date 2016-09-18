1450856483de45bcc557256729ac397bf87a4684
Augusto Henrique Salla Rosa <guto_ivp@hotmail.com>
2016-09-18 15:49:00 -0300

    oi

---
+++ DB1.AvaliacaoTecnica/DB1.AvaliacaoTecnica/DB1.AvaliacaoTecnica/DB1.AvaliacaoTecnica.UI/app/routes.js
@@ -0,0 +1,10 @@
+﻿angular.module('Db1Avaliacao', ['ngRoute'])
+  .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider
+        .when(
+          '/exercicio1', {
+              controller: 'ExercicioCtrl',
+              templateUrl: 'Views/Exercicio1.html'
+          }
+        )
+  }]);
